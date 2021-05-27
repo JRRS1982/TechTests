f0c233966a5ec687e738b0bf491177fee67b097a
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2021-05-27 20:25:13 +0100

    gilded-rose: tests pass after refactor
    did that pretty quick, so imposter syndrome is triggered

--- gilded-rose/src/gilded-rose.ts
+++ gilded-rose/src/gilded-rose.ts
@@ -30,66 +30,36 @@ export class GildedRose {
           this.items[i].sellIn--;
           break;
         case "Backstage passes to a TAFKAL80ETC concert":
+          let beforeQuality = this.items[i].quality;
+          this.items[i].quality = this.items[i].quality + 1;
+          if (this.items[i].sellIn <= 10) {
+            this.items[i].quality = this.items[i].quality + 1;
+          }
+          if (this.items[i].sellIn <= 5) {
+            this.items[i].quality = this.items[i].quality + 1;
+          }
+          if (this.items[i].sellIn <= 0) {
+            this.items[i].quality = 0;
+          }
+          if (this.items[i].quality > 50) {
+            this.items[i].quality = beforeQuality;
+          }
+          this.items[i].sellIn--;
           break;
         case "Sulfuras, Hand of Ragnaros":
           // do nothing
           break;
         default:
-          if (this.items[i].sellIn > 0) {
-
+          if (this.items[i].quality > 0) {
+            this.items[i].quality--;
           }
+          if (this.items[i].sellIn < 0) {
+            this.items[i].quality--;
+          }
+          this.items[i].sellIn--;
           break;
       }
-    
     }
-
-
-
-    // for (let i = 0; i < this.items.length; i++) {
-    //   if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-    //     if (this.items[i].quality > 0) {
-    //       if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-    //         this.items[i].quality = this.decrementQuality(this.items[i]);
-    //       }
-    //     }
-    //   } else {
-    //     if (this.items[i].quality < 50) {
-    //       this.items[i].quality = this.incrementQuality(this.items[i]);;
-    //       if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-    //         if (this.items[i].sellIn < 11) {
-    //           if (this.items[i].quality < 50) {
-    //             this.items[i].quality = this.incrementQuality(this.items[i]);
-    //           }
-    //         }
-    //         if (this.items[i].sellIn < 6) {
-    //           if (this.items[i].quality < 50) {
-    //             this.items[i].quality = this.incrementQuality(this.items[i]);
-    //           }
-    //         }
-    //       }
-    //     }
-    //   }
-    //   if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-    //     this.items[i].sellIn = this.decrementSellIn(this.items[i]);
-    //   }
-    //   if (this.items[i].sellIn < 0) {
-    //     if (this.items[i].name != "Aged Brie") {
-    //       if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-    //         if (this.items[i].quality > 0) {
-    //           if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-    //             this.items[i].quality = this.decrementQuality(this.items[i]);
-    //           }
-    //         }
-    //       } else {
-    //         this.items[i].quality = this.items[i].quality - this.items[i].quality;
-    //       }
-    //     } else {
-    //       if (this.items[i].quality < 50) {
-    //         this.items[i].quality = this.incrementQuality(this.items[i]);
-    //       }
-    //     }
-    //   }
-    // }
 
     return this.items;
   }
